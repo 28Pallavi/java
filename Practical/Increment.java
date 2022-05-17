70cf9b69e93d82f98b53968231e75f997f54c602
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-17 15:59:13 +0530

    Add files via upload

---
+++ Practical/Increment.java
@@ -0,0 +1,17 @@
+package increment;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		int p=10;
+		int q=20;
+		int r;
+		int s;
+		r=++q;
+		s=p++;
+		r++;
+		System.out.println("p= " +p +" q= " +q +" r= " +r +" s= " +s);
+
+	}
+
+}
