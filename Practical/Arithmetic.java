ae4ab2711e4e670a67f3f202118e39955410668c
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-18 16:01:08 +0530

    Add files via upload

---
+++ Practical/Arithmetic.java
@@ -0,0 +1,17 @@
+package arith;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		
+		System.out.println(a+b); 
+		System.out.println(a-b);  
+		System.out.println(a*b); 
+		System.out.println(a/b); 
+		System.out.println(a%b); 
+
+	}
+
+}
