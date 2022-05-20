be31c8d60335e630803ed07a91f5d04be6c3b14d
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-20 16:08:21 +0530

    Add files via upload

---
+++ Practical/Demo.java
@@ -0,0 +1,13 @@
+package no_arg;
+
+public class Demo {
+	public Demo()
+	{
+		System.out.println("Constructor is no argument type.");
+	}
+
+	public static void main(String[] args) {
+		new Demo();
+	}
+
+}
