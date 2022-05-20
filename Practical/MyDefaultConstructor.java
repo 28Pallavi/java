777246477c48baf583b70166cadc19751fc42b84
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-20 16:04:17 +0530

    Add files via upload

---
+++ Practical/MyDefaultConstructor.java
@@ -0,0 +1,14 @@
+package com.myjava.constructors;
+
+public class MyDefaultConstructor
+{
+	public MyDefaultConstructor()
+	{
+		System.out.println("This is default constructor.");
+	}
+
+	public static void main(String[] args) {
+		MyDefaultConstructor mdc = new MyDefaultConstructor();
+	}
+
+}
