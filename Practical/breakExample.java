e6e6fd67c9b669400ebe2ced62795725e0ae6563
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-20 20:45:03 +0530

    Add files via upload

---
+++ Practical/breakExample.java
@@ -0,0 +1,17 @@
+package breakEx;
+
+public class breakExample {
+
+	public static void main(String[] args)
+	{
+		for(int i=2;i<=8;i++)
+		{
+			if(i==6)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
