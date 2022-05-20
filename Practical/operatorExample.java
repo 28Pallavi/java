94407d2b8e0407ee3d6d7d1ccf09d2ea9e12ccf3
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-20 16:24:00 +0530

    Add files via upload

---
+++ Practical/operatorExample.java
@@ -0,0 +1,16 @@
+package operatorEx;
+
+public class operatorExample {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=3;
+		
+		System.out.println(a+b); 
+		System.out.println(a-b); 
+		System.out.println(a*b); 
+		System.out.println(a/b);  
+	}
+
+}
