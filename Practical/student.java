2f5ca9e1027d79769fe0f646344425331ff26e83
28Pallavi <105425792+28Pallavi@users.noreply.github.com>
2022-05-20 15:37:42 +0530

    Add files via upload

---
+++ Practical/student.java
@@ -0,0 +1,32 @@
+package constrOverloading;
+
+public class student {
+	
+	int id;
+	String name;
+	int age;
+	student(int i,String n){
+	id = i;
+	name = n;
+	}
+	student(int i,String n,int a){
+	id = i;
+	name = n;
+	age=a;
+	}
+	
+	void display()
+	{
+		System.out.println(id+" "+name+" "+age);
+	}
+	
+
+	public static void main(String[] args) {
+		student s1 = new student(802,"Pallavi",21);
+		student s2 = new student(810,"Neha",22);
+		s1.display();
+		s2.display();
+		}
+}
+
+
