42094a922360413990de4db59c3b771a5c8d8737
Pallavi Humbad <105425792+28Pallavi@users.noreply.github.com>
2022-07-11 17:55:18 +0530

    Add files via upload

---
+++ Practical/Student.java
@@ -0,0 +1,42 @@
+package com;
+
+public class Student {
+	
+	//declaring variables as private 
+	private int id;
+	private String sName;
+	private String SEmail;
+	private long sPhone;
+	
+	//getter and setter methods for accessing private variables
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getSEmail() {
+		return SEmail;
+	}
+	public void setSEmail(String sEmail) {
+		SEmail = sEmail;
+	}
+	public long getsPhone() {
+		return sPhone;
+	}
+	public void setsPhone(long sPhone) {
+		this.sPhone = sPhone;
+	}
+	
+	
+	
+	
+
+}
