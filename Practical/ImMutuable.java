c1236aee9ac6bcee3ec586aa695f938062262b5c
Pallavi Humbad <105425792+28Pallavi@users.noreply.github.com>
2022-07-18 18:18:52 +0530

    Add files via upload

---
+++ Practical/ImMutuable.java
@@ -0,0 +1,12 @@
+package com;
+
+public class ImMutuable {
+
+	public static void main(String[] args) {
+		String s = "Pallavi";
+		s.concat("Humbad"); //append string at end
+		System.out.println(s); //print only Pallavi as string object is immutable
+
+	}
+
+}
