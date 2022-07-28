d98443e6cdbaef7f255ee4eae322b7f1df760769
Pallavi Humbad <105425792+28Pallavi@users.noreply.github.com>
2022-07-28 16:54:00 +0530

    Add files via upload

---
+++ lab activity/ArrayElement.java
@@ -0,0 +1,17 @@
+package lab;
+
+public class ArrayElement {
+
+	public static void main(String[] args) {
+		
+		int [] arr = {1,2,3,4,5}; //creating array
+		
+		System.out.println("Array elements in odd position: ");
+		
+		for(int i=0;i<arr.length;i+=2) { //for loop for printing odd position array element
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
