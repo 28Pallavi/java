54d76c2a84ce601e0688f6262cd7be66f6cb16ab
Pallavi Humbad <105425792+28Pallavi@users.noreply.github.com>
2022-09-15 17:55:24 +0530

    Add files via upload

---
+++ lab activity/TrainerDao.java
@@ -0,0 +1,9 @@
+package com.HbOnetoManyUni.dao;
+
+public interface TrainerDao {
+
+	public void addTrainer(); //save new trainer details im db
+	
+	public void fetchTrainer(); //fetch existing trainer details in db
+
+}
