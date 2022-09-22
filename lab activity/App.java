1c5de32595a7ca3f028e4ca164517f9979f7fea3
Pallavi Humbad <105425792+28Pallavi@users.noreply.github.com>
2022-09-22 17:17:51 +0530

    Add files via upload

--- lab activity/App.java
+++ lab activity/App.java
@@ -1,14 +1,47 @@
-package com.HbOnetoManyUni;
+package com.jpa;
 
-import com.HbOnetoManyUni.daoimpl.TrainerDaoImpl;
+import java.util.Scanner;
+
+import com.jpa.daoimpl.CustomerDaoImpl;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        TrainerDaoImpl d = new TrainerDaoImpl();
-        d.addTrainer();
-        d.fetchTrainer();
-        
+        CustomerDaoImpl cdao = new CustomerDaoImpl();
+        char a;
+        do {
+        	@SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+        	System.out.println("Customer Registration");
+        	System.out.println("Enter your choice");
+        	System.out.println("1.Add \n 2.Read \n 3.Update \n 4.Delete \n 5.Exit");
+        	int ch = sc.nextInt();
+        	
+        	switch(ch) {
+        	
+        	case 1 : cdao.addCustomer();
+        	break;
+        	
+        	case 2 : cdao.getCustomerBy();
+        	break;
+        	
+        	case 3 : cdao.updateCustomer();
+        	break;
+        	
+        	case 4 : cdao.deleteCustomer();
+        	break;
+        	
+        	case 5 : 
+        		System.exit(0);
+        		break;
+        		default: 
+        			System.out.println("invalid choice");
+        	}
+        	System.out.println("Do you want to continue ? Y/N");
+        	a=sc.next().charAt(0);
+        }
+        while(a=='Y' || a=='y');
+        System.out.println("Thank you");
     }
 }
